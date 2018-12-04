a4f1d60c79dc90661ec9d791533e8adba03f0750
Worler94 <guglielminiluca94@libero.it>
2018-12-04 16:12:47 +0100

    ho aggiornato tutto

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  ricetta: Recipe = new Recipe('Patate',
+    'lorem ipsum',
+    'https://www.lalampadina.net/magazine/wp-content/uploads/2016/02/cibi-spazzatura.jpg');
+  ricetta2: Recipe = new Recipe('Tate',
+    'orem ipsum',
+    'https://www.lalampadina.net/magazine/wp-content/uploads/2016/02/cibi-spazzatura.jpg');
+  ricetta3: Recipe = new Recipe('dhsfbv<hab',
+    'rem ipsum',
+    'https://www.lalampadina.net/magazine/wp-content/uploads/2016/02/cibi-spazzatura.jpg');
+  recipes: Recipe[] = [
+    this.ricetta,
+    this.ricetta2,
+    this.ricetta3
+  ];
+  ingredients = {
+    nome: 'Aglio',
+    peso: '5g'
+  };
   constructor() { }
 
   ngOnInit() {
